23df726d9f3b107969a2922f353d118b0e5342e4
reng <reng412@gmail.com>
2021-01-15 17:49:27 -0800

    Add searchbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Jumbotron from "./components/Jumbotron";
+import Searchbar from "./components/Searchbar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Jumbotron />
+      <Searchbar />
     </div>
   );
 }
---
+++ client/src/components/Searchbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FormGroup, Button, Input, Container, Form, Label } from 'reactstrap';
+
+const Searchbar = (props) => {
+    return (
+        <Container>
+            <Form>
+                <FormGroup>
+                    <Label for="movieTitle">Enter a movie title to select it for the Movie Awards nomination.</Label>
+                    <Input type="text" name="movieTitle" id="movieTitle" placeholder="Movie Title" />
+                </FormGroup>
+                <Button onClick={props.submit} type="submit">Search</Button>
+            </Form>
+        </Container>
+    );
+};
+
+export default Searchbar;
