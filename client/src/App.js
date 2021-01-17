255928da1fc738a4473867b7dfa1c48336c21e01
reng <reng412@gmail.com>
2021-01-16 16:46:21 -0800

    Add ternary operator to display component with movie search results if successful

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import API from "./utils/API";
 function App() {
   const [search, setSearch] = useState("");
   const [movieResults, setMovieResults] = useState({});
+  const [searchStatus, setSearchStatus] = useState(false);
 
   // Function to track input change in searchbar
   const handleInputChange = e => {
@@ -23,13 +24,18 @@ function App() {
     // If there is a movie input, search through the OMBD API
     API.search(search)
       .then(res => {
-        if (res.Error) {
-          throw new Error("No results found.")
+        // If there is a movie result from OMDB, display set search status to true, otherwise set to false
+        if (res.data.Response === "True") {
+          setSearchStatus(true);
+          setMovieResults(res.data);
+          console.log(res.data);
+        } else {
+          setSearchStatus(false);
         }
-        setMovieResults(res.data);
-        console.log(res.data);
       })
       .catch(err => console.log(err));
+
+    setSearch("");
   };
 
   return (
@@ -37,7 +43,11 @@ function App() {
     <div className="App">
       <Jumbotron />
       <Searchbar inputChange={handleInputChange} search={search} formSubmit={handleFormSubmit} />
-      <MovieCard movie={movieResults} />
+      {/* If the search status is true display the results in a movie card component, otherwise display empty div*/}
+      { searchStatus ?
+        <MovieCard movie={movieResults} /> :
+        <p></p>
+      }
     </div>
   );
 }
