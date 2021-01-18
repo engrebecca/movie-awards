b5a429eaf943d077f0d41bb1764d3b50484da1c7
reng <reng412@gmail.com>
2021-01-17 16:51:27 -0800

    Deploy

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -23,4 +23,11 @@ app.get("/movies", async (req, res) => {
     res.json(movies);
 })
 
+// Return all requests to React app
+if (process.env.NODE_ENV === "production") {
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/build/index.html"))
+    })
+}
+
 module.exports = app;
